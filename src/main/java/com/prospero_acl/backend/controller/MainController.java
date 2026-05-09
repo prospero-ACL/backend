06bbd8c36a6f61554382c2e0b325da3df33d4a9b
Themis Darelis <thedarelis@gmail.com>
2026-05-09 16:17:58 +0300

    set up connection to db

---
+++ src/main/java/com/prospero_acl/backend/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.prospero_acl.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1")
+public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index";
+  }
+
+}
---
+++ src/main/java/com/prospero_acl/backend/repo/VectorDatabase.java
@@ -0,0 +1,24 @@
+package com.prospero_acl.backend.repo;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgDistanceType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class VectorDatabase {
+
+  @Bean
+  public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
+    return PgVectorStore
+        .builder(jdbcTemplate, embeddingModel)
+        .dimensions(1536)// better to specify dimensions
+        .distanceType(PgDistanceType.COSINE_DISTANCE)// this is optional
+        .indexType(PgVectorStore.PgIndexType.HNSW)// this is optional
+
+        .build();
+  }
+}
