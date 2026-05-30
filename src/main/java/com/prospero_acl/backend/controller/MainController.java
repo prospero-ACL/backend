5641f316786ca70effb7a196c99ea54400463ac2
Themis Darelis <thedarelis@gmail.com>
2026-05-30 19:22:20 +0300

    started document service

--- src/main/java/com/prospero_acl/backend/controller/MainController.java
+++ src/main/java/com/prospero_acl/backend/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.prospero_acl.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class MainController {
   @GetMapping("/test")
   public String getHello() {
     return "Hello World";
+  }
+
+  @PostMapping("/upload")
+  public void storeDocument() {
+
   }
 
 }
